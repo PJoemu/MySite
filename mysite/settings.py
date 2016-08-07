47d969624b9abf51deb83e924a3d299cb9317be8
Joemu <huangxing654123@163.com>
2016-08-07 14:04:56 +0800

    解决static问题

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,5 +88,4 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'blog', 'static'),
 )
