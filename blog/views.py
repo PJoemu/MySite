7bd347501314cc879741c1fee06787b2b23b49ee
Joemu <huangxing654123@163.com>
2016-08-07 15:54:27 +0800

    格式化

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
 
 
 @login_required(login_url='/login/')
-def home(request):
-    return render_to_response('index.html')
-
-
 def portal(request):
     return render_to_response('portal.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from blog import views
+from blog import views, urls as blog_urls
 
 urlpatterns = patterns('',
                        # Examples:
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^blog/', include(blog_urls)),
                        url(r'^$', views.portal, name='portal'),
                        url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
                        url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'login.html'}),
-                       url(r'^home/$', views.home, name='home'),
                        )
